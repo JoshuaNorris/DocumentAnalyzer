059dd5953b5b01ba463152333c30d7f9f020754d
Colby-Hall <hallcc@hendrix.edu>
2018-04-19 23:29:32 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/JoshuaNorris/DocumentAnalyzer""
    This reverts commit fcacd1ad834a4a6af2662492ebbac24c9dfb9062.

--- src/Summarizer/ScoreSummarizer.java
+++ src/Summarizer/ScoreSummarizer.java
@@ -11,11 +11,12 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import edu.stanford.nlp.simple.Document;
-import edu.stanford.nlp.simple.Sentence;
-import edu.stanford.nlp.simple.Token;
+//import edu.stanford.nlp.simple.Document;
+//import edu.stanford.nlp.simple.Sentence;
+//import edu.stanford.nlp.simple.Token;
 
 public class ScoreSummarizer {
+<<<<<<< HEAD
 
 	private ArrayList<Sentence> allSentences = new ArrayList<Sentence>();
 	private ArrayList<Integer> scores = new ArrayList<Integer>();
@@ -24,6 +25,12 @@ public class ScoreSummarizer {
 
 	public ScoreSummarizer() {
 	}
+=======
+/**
+	private LinkedHashMap<Sentence, Double> pileOfSentences = new LinkedHashMap<Sentence, Double>();
+	private ArrayList<String> ignoreWords;
+	private ArrayList<Sentence> finalSentences;
+>>>>>>> b29ebcdcc742386abf26f00c4a4c666635aa3f54
 
 	public void stopMaker() throws IOException {
 		File file = new File("C:\\Users\\colby\\Documents\\CSCI 270 Computational Humanities\\stop-words-list.txt");
@@ -61,8 +68,12 @@ public class ScoreSummarizer {
 		System.out.println(scores);
 		System.out.println(bestScoring);
 
+<<<<<<< HEAD
 
 
 	}
+=======
+	//} */
+>>>>>>> b29ebcdcc742386abf26f00c4a4c666635aa3f54
 
 }
