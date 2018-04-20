69783e5abd980c4db24cd25931c9a362dc57f58a
Colby-Hall <hallcc@hendrix.edu>
2018-04-19 23:01:54 -0500

    Need to show best sentences

--- src/Summarizer/ScoreSummarizer.java
+++ src/Summarizer/ScoreSummarizer.java
@@ -17,37 +17,52 @@ import edu.stanford.nlp.simple.Token;
 
 public class ScoreSummarizer {
 
-	private LinkedHashMap<Sentence, Double> pileOfSentences = new LinkedHashMap<Sentence, Double>();
-	private ArrayList<String> ignoreWords;
-	private ArrayList<Sentence> finalSentences;
+	private ArrayList<Sentence> allSentences = new ArrayList<Sentence>();
+	private ArrayList<Integer> scores = new ArrayList<Integer>();
+	private ArrayList<String> ignoreWords = new ArrayList<String>();
+	private ArrayList<Sentence> finalSentences = new ArrayList<Sentence>();
+
+	public ScoreSummarizer() {
+	}
 
 	public void stopMaker() throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader("stop-words-list.txt"));
+		File file = new File("C:\\Users\\colby\\Documents\\CSCI 270 Computational Humanities\\stop-words-list.txt");
+		BufferedReader reader = new BufferedReader(new FileReader(file));
 		while (reader.readLine() != null) {
 			ignoreWords.add(reader.readLine());
 		}
-		reader.close();
+
 	}
 
 	public void ScoreSentences(String text) {
+		int score = 0;
 		Document userText = new Document(text);
 		List<Sentence> sentences = userText.sentences();
 		for (Sentence potential : sentences) {
-			pileOfSentences.put(potential, 0.0);
+			allSentences.add(potential);
 			for (Token word : potential.tokens()) {
 				String scoreWord = word.word();
 				if (!ignoreWords.contains(scoreWord)) {
-					pileOfSentences.put(potential, pileOfSentences.get(potential) + 1);
+					score += 1;
 				}
 
 			}
-
+			scores.add(score);
+			score = 0;
 		}
 
 	}
 
-	//public ArrayList topReturner() {
+	public void topReturner() {
+		ArrayList<Integer> bestScoring = new ArrayList<Integer>();
+		bestScoring.addAll(scores);
+		bestScoring.sort(null);
+		System.out.println(allSentences);
+		System.out.println(scores);
+		System.out.println(bestScoring);
 
-	//}
+
+
+	}
 
 }
